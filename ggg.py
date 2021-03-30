8b2382c654eaca43090b77500927da6d3a570357
Christian Alvarez <alvarez.christian46@outlook.com>
2021-03-30 13:22:33 -0500

    #

---
+++ ggg.py
@@ -0,0 +1,8 @@
+from PyQt5 import QtWidgets, uic
+import sys
+
+QtGui.QMessageBox.question(self, 
+         'Message',
+         "Are you sure to quit?",
+         QtGui.QMessageBox.Yes, 
+         QtGui.QMessageBox.No)
---
+++ rpcdir/ygl.py
@@ -0,0 +1,6 @@
+def theFall():
+    bs = ",,,,.....wdwwdw...ede...def.e...e.ezoom...wdq,,.w"
+    ds = bs.strip(",.wdeq")
+    return ds
+
+theFall()
